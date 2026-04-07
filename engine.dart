bcc7f147302edc2ec970a960b7d3d410b50681b4
treeplate <tree.plate.moo@gmail.com>
2026-04-07 14:59:30 -0700

    - upgrade to dart 3 - format - fix some pretty significant bugs (like i don't think the old version even compiled)

--- engine.dart
+++ engine.dart
@@ -17,21 +17,20 @@ Room position = bedroom;
 final List<Thing> inventory = <Thing>[];
 List<Room> rooms = <Room>[];
 bool TOS = false;
-String roomone;
+late Room roomone;
+Room thingstore = Room('thingstore', 'temporary room for things');
+
 class Room {
   String name;
   String description;
   String jump;
-  Room _e;
-  Room _w;
-  Room _s;
-  Room _n;
-  Room down;
-  Room up;
-  Room(String iname,String idescription,{String ijump : 'boiing'}){
-    name = iname;
-    description=idescription;
-    jump = ijump;
+  late Room _e;
+  late Room _w;
+  late Room _s;
+  late Room _n;
+  late Room down;
+  late Room up;
+  Room(this.name, this.description, {this.jump = 'boiing'}) {
     _n = this;
     _s = this;
     _e = this;
@@ -43,20 +42,22 @@ class Room {
   void add(Thing adding) {
     inventory.add(adding);
   }
+
   void remove(Thing removing) {
     inventory.remove(removing);
   }
+
   final Set<Thing> inventory = new Set<Thing>();
 
-  Room n()  {
-    if (_n == this){
+  Room n() {
+    if (_n == this) {
       print('No effect.');
     }
     return _n;
   }
 
   Room e() {
-    if (_e == this){
+    if (_e == this) {
       print('No effect.');
     }
 
@@ -64,14 +65,15 @@ class Room {
   }
 
   Room w() {
-    if (_w == this){
+    if (_w == this) {
       print('No effect.');
     }
 
     return _w;
   }
+
   Room s() {
-    if (_s == this){
+    if (_s == this) {
       print('No effect.');
     }
 
@@ -80,46 +82,43 @@ class Room {
 }
 
 class Thing {
-  Thing(this.name, Room position){
-   position.add(this); 
+  Thing(this.name, Room position) {
+    position.add(this);
   }
-   final String name;
-  void moveTo(Room current, Room movingTo){
+  final String name;
+  void moveTo(Room current, Room movingTo) {
     current.remove(this);
     movingTo.add(this);
   }
-  bool canMove(){
+
+  bool canMove() {
     return true;
   }
 }
-class HeavyObject extends Thing{
-  HeavyObject(String name, Room position):super(name, position);
-  bool canMove(){
+
+class HeavyObject extends Thing {
+  HeavyObject(String name, Room position) : super(name, position);
+  bool canMove() {
     return false;
   }
 }
 
-void connect(String dir, Room one, Room two){
-  if (dir == 'n'){
+void connect(String dir, Room one, Room two) {
+  if (dir == 'n') {
     one._n = two;
     two._s = one;
-  }
-  else if (dir == 's'){
+  } else if (dir == 's') {
     connect('n', two, one);
-  }
-  else if (dir == 'w'){
+  } else if (dir == 'w') {
     one._w = two;
     two._e = one;
-  }
-  else if (dir == 'e'){
+  } else if (dir == 'e') {
     connect('w', two, one);
-  }
-  else if (dir == 'down'){
+  } else if (dir == 'down') {
     one.down = two;
     two.up = one;
-  }
-  else if (dir == 'up'){
-    connect('down',two, one); 
+  } else if (dir == 'up') {
+    connect('down', two, one);
   }
 }
 
@@ -131,157 +130,152 @@ void main() {
   print('Type "commands" for a list of commands.');
   while (!finished) {
     print('debug: Rooms');
-    for (Room room in rooms){
+    for (Room room in rooms) {
       print('\'${room.name}\'');
     }
-    print (position.name);
-    print (position.description);
+    print(position.name);
+    print(position.description);
     if (position.inventory.isNotEmpty) {
       print('You can see the');
-      for (Thing thing in position.inventory){
-      print(thing.name);
+      for (Thing thing in position.inventory) {
+        print(thing.name);
       }
     }
     if (inventory.isNotEmpty) {
       print('\nYou have the');
-      for (Thing thing in inventory){
-      print(thing.name);
+      for (Thing thing in inventory) {
+        print(thing.name);
       }
     }
     print('\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv');
-    String command = stdin.readLineSync();
+    String command = stdin.readLineSync()!;
     commanddeal('$command');
   }
 }
-void commanddeal(String command){
-    if (command == jump) {
-      print (position.jump);
-    } else if (command == north) {
-      position = position.n();
-    } else if (command == south) {
-      position = position.s();
-    } else if (command == west) {
-      position = position.w();
-    } else if (command == east) {
-      position = position.e();
-    } else if (command == 'down'){
-      if (position == position.down){
-        print('No Effect');
-      }
-      position = position.down;
-    } else if (command == 'up'){
-      if (position == position.up){
-        print('No Effect');
-      }
-      position = position.up;      
-    } else if (command == quit) {
-      print('Good bye!');
-      finished = true;
-    } else if (command == commands) {
-	print('Commands:\ns = Go south\nn = Go north\ne = Go east\nw = Go west\nquit = Quit this game\njump = Jump\nsleep = Take a nap if there\'s a bed in your current room\ntake (object): Take (object) if you can\ndrop (object): Drop (object) if you can\n\nall where it says (object) does it for all objects that you are able to use for (object)');
-    } else if (command == sleep && position == bedroom){
-        print('Zzzzz'); 
-    } else if (command.startsWith('take all')) {
-      print('take all');
-      while (position.inventory.isNotEmpty){
-        Thing thing = position.inventory.first;
-        commanddeal('take ${thing.name}');
-        if (TOS == true){
-          position.remove(thing);
-          notTaking.add(thing);
-        }
-      }
-      while(notTaking.isNotEmpty){
-        Thing thing = notTaking.first;
-        position.add(thing);
-        notTaking.remove(thing);
-      }      
-    } else if (command.startsWith('drop all')) {
-       while (inventory.isNotEmpty){
-         Thing thing = inventory.first;
-         inventory.remove(thing);
-         position.add(thing);
-         print('You now don\'t have the ${thing.name}');
-       }
 
-
-    } else if (command.startsWith('take ')) {
-      for (Thing thing in position.inventory){
-        if (command.substring(5) == thing.name) {
-          if (thing.canMove() == true){
-            inventory.add(thing);
-            position.remove(thing);
-            print('You now have the ${thing.name}');
-            TOS = false;
-            break;
-          }else{
-            print('The ${thing.name} is too heavy');
-            TOS = true; 
-          }
-        }  
+void commanddeal(String command) {
+  if (command == jump) {
+    print(position.jump);
+  } else if (command == north) {
+    position = position.n();
+  } else if (command == south) {
+    position = position.s();
+  } else if (command == west) {
+    position = position.w();
+  } else if (command == east) {
+    position = position.e();
+  } else if (command == 'down') {
+    if (position == position.down) {
+      print('No Effect');
+    }
+    position = position.down;
+  } else if (command == 'up') {
+    if (position == position.up) {
+      print('No Effect');
+    }
+    position = position.up;
+  } else if (command == quit) {
+    print('Good bye!');
+    finished = true;
+  } else if (command == commands) {
+    print(
+        'Commands:\ns = Go south\nn = Go north\ne = Go east\nw = Go west\nquit = Quit this game\njump = Jump\nsleep = Take a nap if there\'s a bed in your current room\ntake (object): Take (object) if you can\ndrop (object): Drop (object) if you can\n\nall where it says (object) does it for all objects that you are able to use for (object)');
+  } else if (command == sleep && position == bedroom) {
+    print('Zzzzz');
+  } else if (command.startsWith('take all')) {
+    print('take all');
+    while (position.inventory.isNotEmpty) {
+      Thing thing = position.inventory.first;
+      commanddeal('take ${thing.name}');
+      if (TOS == true) {
+        position.remove(thing);
+        notTaking.add(thing);
       }
-    } else if (command.startsWith('drop ')) {
-       for (Thing thing in inventory){
-         if (command.substring(5) == thing.name) {
-           inventory.remove(thing);
-           position.add(thing);
-           print('You now don\'t have the ${thing.name}');
-           break;
-         }
-       }
-    } else if (command.startsWith('take all')) {
-      print('take all');
-      while (position.inventory.isNotEmpty){
-        Thing thing = position.inventory.first;
-        if (thing.canMove() == true){
+    }
+    while (notTaking.isNotEmpty) {
+      Thing thing = notTaking.first;
+      position.add(thing);
+      notTaking.remove(thing);
+    }
+  } else if (command.startsWith('drop all')) {
+    while (inventory.isNotEmpty) {
+      Thing thing = inventory.first;
+      inventory.remove(thing);
+      position.add(thing);
+      print('You now don\'t have the ${thing.name}');
+    }
+  } else if (command.startsWith('take ')) {
+    for (Thing thing in position.inventory) {
+      if (command.substring(5) == thing.name) {
+        if (thing.canMove() == true) {
           inventory.add(thing);
           position.remove(thing);
           print('You now have the ${thing.name}');
+          TOS = false;
+          break;
         } else {
-          print('You can\'t take the ${thing.name}, it\'s too heavy!');
-          position.remove(thing);
-          notTaking.add(thing);
-        } 
-      }
-      while(notTaking.isNotEmpty){
-        Thing thing = notTaking.first;
-        position.add(thing);
-        notTaking.remove(thing);
-      }
-    } else if (command.startsWith('make ')) {
-      new Thing(command.substring(5), position);
-    } else if (command.startsWith('heavymake ')) {
-      new HeavyObject(command.substring(10), position);
-      
-    } else if (command.startsWith('destroy ')) {
-      for (Thing thing in position.inventory){
-        if (thing.name == command.substring(8)){
-          thing.moveTo(position, thingstore);
-          break;
-        }
-      } 
-    } else if(command.startsWith('new Room ')) {
-      new Room(command.substring(9),'This is a custom room');
-    } else if(command == 'connection'){
-      String roomer = stdin.readLineSync();    
-      for (Room room in rooms){
-        if (roomer.startsWith(room.name)){
-          roomone = room;
-          break;
+          print('The ${thing.name} is too heavy');
+          TOS = true;
         }
       }
-      for (Room room in rooms){
-        if (roomer.substring(roomone.name.length + 1) == (room.name)){
-          String dir = stdin.readLineSync();
-          connect(stdin.readLineSync(), roomone, room);
-          break;
-        }
-      }
-
-    } else {
-      print('Quoi?');
     }
-  
+  } else if (command.startsWith('drop ')) {
+    for (Thing thing in inventory) {
+      if (command.substring(5) == thing.name) {
+        inventory.remove(thing);
+        position.add(thing);
+        print('You now don\'t have the ${thing.name}');
+        break;
+      }
+    }
+  } else if (command.startsWith('take all')) {
+    print('take all');
+    while (position.inventory.isNotEmpty) {
+      Thing thing = position.inventory.first;
+      if (thing.canMove() == true) {
+        inventory.add(thing);
+        position.remove(thing);
+        print('You now have the ${thing.name}');
+      } else {
+        print('You can\'t take the ${thing.name}, it\'s too heavy!');
+        position.remove(thing);
+        notTaking.add(thing);
+      }
+    }
+    while (notTaking.isNotEmpty) {
+      Thing thing = notTaking.first;
+      position.add(thing);
+      notTaking.remove(thing);
+    }
+  } else if (command.startsWith('make ')) {
+    new Thing(command.substring(5), position);
+  } else if (command.startsWith('heavymake ')) {
+    new HeavyObject(command.substring(10), position);
+  } else if (command.startsWith('destroy ')) {
+    for (Thing thing in position.inventory) {
+      if (thing.name == command.substring(8)) {
+        thing.moveTo(position, thingstore);
+        break;
+      }
+    }
+  } else if (command.startsWith('new Room ')) {
+    new Room(command.substring(9), 'This is a custom room');
+  } else if (command == 'connection') {
+    String roomer = stdin.readLineSync()!;
+    for (Room room in rooms) {
+      if (roomer.startsWith(room.name)) {
+        roomone = room;
+        break;
+      }
+    }
+    for (Room room in rooms) {
+      if (roomer.substring(roomone.name.length + 1) == (room.name)) {
+        String dir = stdin.readLineSync()!;
+        connect(dir, roomone, room);
+        break;
+      }
+    }
+  } else {
+    print('Quoi?');
+  }
 }
-
-
